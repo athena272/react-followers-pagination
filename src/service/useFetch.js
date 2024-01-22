d421634b192ac7bfaf1f6cbac4a5ccbe1279260d
athena272 <guilhermera272@gmail.com>
2024-01-22 13:47:52 -0300

    Update useFetch.js

--- src/service/useFetch.js
+++ src/service/useFetch.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import paginate from './utils'
-const url = 'https://api.github.com/users/athena272/followers'
+const url = 'https://api.github.com/users/athena272/followers?per_page=100'
 
 export const useFetch = () => {
     const [loading, setLoading] = useState(true)
